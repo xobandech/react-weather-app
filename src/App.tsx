d35c05b2ec191cc08ff34982a0f1466b76bf50de
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-06-08 19:28:50 +0200

    Created react app with tsx template

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { MONTHS, DAYS } from './constants/constants';
+const api = {
+  key: "63b7ee299beeec17b7277ca1cd58d2f3",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
+  const [location, setLocation] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = async evt => {
+    if (evt.key === "Enter") {
+      const res = await fetch(`${api.base}weather?q=${location}&units=metric&APPID=${api.key}`)
+      const jsonRes = await res.json()
+      setWeather(jsonRes);
+      setLocation('');
+      console.log(weather);
+    }
+  }
+
+  const dateBuilder = (d) => {
+    const day = DAYS[d.getDay()];
+    const date = d.getDate();
+    const month = MONTHS[d.getMonth()];
+    const year = d.getFullYear();
+    
+    return `${day} ${date} ${month} ${year}`
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
+      <main>
+        <div className="search-box">
+          <input 
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={e => setLocation
+          (e.target.value)}
+            value={location}
+            onKeyPress={search}
+          />
+        </div>
+        {(typeof weather.main != "undefined") ? (
+        <div>
+          <div className="location-box">
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="date">{dateBuilder(new Date())}</div>
+          </div>
+          <div className="weather-box">
+            <div className="temp">
+              {Math.round(weather.main.temp)}°c
+            </div>
+            <div className="weather">{weather.weather[0].description}</div>
+          </div>
+        </div>
+        ) : ('')}
+      </main>
     </div>
   );
 }
